bf3580e30607e62a4ded543ff5bfe5e570df6172
JAEHO LEE <spectrick@gmail.com>
2013-12-02 01:24:33 +0900

    bootstrap patch & remove iron-router

--- blog.js
+++ blog.js
@@ -1,14 +1,4 @@
 if (Meteor.isClient) {
-  Router.configure({
-    layoutTemplate: 'layout'
-  });
-
-  Router.map(function() {
-    this.route('post', {
-      path: '/',
-      template: 'post'
-    });
-  });
   Template.post.rendered= function() {
     var editor = new MediumEditor('.editable');
     $('.editable').mediumImages({'uploadScript': 'upload'});
@@ -16,23 +6,18 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-  Router.map(function() {
-    this.route('upload', {
-      where: 'server',
-      path: '/upload',
-      action: function() {
-        console.log(this.request.files.file.path);
-        var uploadPath = this.request.files.file.path;
-        var mime = Meteor.require('mime');
-        var fs = Npm.require('fs');
-        this.response.end(
-          'data:'+mime.lookup(uploadPath)+';base64,'+
+  var mime = Meteor.require('mime');
+  var fs = Npm.require('fs');
+  WebApp.connectHandlers.use(function(req, res, next) {
+    if (req.url==='/upload') {
+      console.log(this.request.files.file.path);
+      var uploadPath = this.request.files.file.path;
+      res.end(
+        'data:'+mime.lookup(uploadPath)+';base64,'+
           ((Meteor._wrapAsync(fs.readFile))(uploadPath)).toString("base64")
-        );
-      }
-    })
+      );
+    } else {
+      next();
+    }
   });
 }
