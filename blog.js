7362a7ef4ebddb4bdadf6137487b4a956d6b1622
JAEHO LEE <spectrick@gmail.com>
2013-12-02 09:22:30 +0900

    iron-router로 다시.

--- blog.js
+++ blog.js
@@ -1,4 +1,14 @@
 if (Meteor.isClient) {
+  Router.configure({
+    layoutTemplate: 'layout'
+  });
+
+  Router.map(function() {
+    this.route('post', {
+      path: '/',
+      template: 'post'
+    });
+  });
   Template.post.rendered= function() {
     var editor = new MediumEditor('.editable');
     $('.editable').mediumImages({'uploadScript': 'upload'});
@@ -6,18 +16,23 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  var mime = Meteor.require('mime');
-  var fs = Npm.require('fs');
-  WebApp.connectHandlers.use(function(req, res, next) {
-    if (req.url==='/upload') {
-      console.log(this.request.files.file.path);
-      var uploadPath = this.request.files.file.path;
-      res.end(
-        'data:'+mime.lookup(uploadPath)+';base64,'+
-          ((Meteor._wrapAsync(fs.readFile))(uploadPath)).toString("base64")
-      );
-    } else {
-      next();
-    }
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+  Router.map(function() {
+    this.route('upload', {
+      where: 'server',
+      path: '/upload',
+      action: function() {
+        console.log(this.request.files.file.path);
+        var uploadPath = this.request.files.file.path;
+        var mime = Meteor.require('mime');
+        var fs = Npm.require('fs');
+        this.response.end(
+          'data:'+mime.lookup(uploadPath)+';base64,'+
+            fs.readFileSync(uploadPath).toString("base64")
+        );
+      }
+    })
   });
 }
